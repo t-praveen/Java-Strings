5e1281e18573eb2bf088b5b81bd897f877a3a9bb
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-28 14:13:13 +0530

    Add files via upload

---
+++ strings/IndexOfMethod2.java
@@ -0,0 +1,16 @@
+package strings;
+
+public class IndexOfMethod2 {
+
+	public static void main(String[] args) {
+
+		String str1 = "Learn Java programming";
+	    int result;
+	    result = str1.indexOf('a', 4);
+	    System.out.println("After implementing the IndexOf method on the following string , the result is : "+result);
+	    result = str1.indexOf("Java", 8);
+	    System.out.println("After implementing the IndexOf method on the following string , the result is : "+result); 
+	  }
+}
+
+
