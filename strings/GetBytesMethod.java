70d2b8682e931e6f5622fd61d737e809e467a3d0
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-28 13:49:42 +0530

    Add files via upload

---
+++ strings/GetBytesMethod.java
@@ -0,0 +1,17 @@
+package strings;
+import java.util.Arrays;
+
+
+public class GetBytesMethod {
+
+	public static void main(String[] args) {
+		
+		String str = "Praveen";
+		byte[] byteArray ;
+		byteArray = str.getBytes();
+		System.out.println(Arrays.toString(byteArray));
+		
+
+	}
+
+}
