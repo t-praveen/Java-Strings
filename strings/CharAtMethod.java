09e0a007f9aa6542d0d4eb15d52679b4014a3ec2
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-28 13:30:00 +0530

    Add files via upload

---
+++ strings/CharAtMethod.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class CharAtMethod {
+
+	public static void main(String[] args) {
+
+		String s = "Hello World!!";
+		String S = "Hello\nWorld";
+		System.out.println("After implementing the CharAt method ,  the first String is : "+s.charAt(0));
+		System.out.println("After implementing the CharAt method ,  the second String is : "+s.charAt(3));
+		System.out.println("After implementing the CharAt method ,  the third String is : "+S.charAt(5));
+		System.out.println("After implementing the CharAt method ,  the fourth String is : "+s.charAt(7));
+	}
+
+}
