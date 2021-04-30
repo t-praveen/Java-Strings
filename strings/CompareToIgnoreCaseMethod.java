edc8b597e57be500eb17dd247a41557379a17ca3
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-04-30 14:38:31 +0530

    Add files via upload

---
+++ strings/CompareToIgnoreCaseMethod.java
@@ -0,0 +1,38 @@
+package strings;
+
+import java.util.Scanner;
+
+public class CompareToIgnoreCaseMethod {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter First String : ");
+		String S1 = sc.nextLine();
+		System.out.print("Enter Second String : ");
+		String S2 = sc.nextLine();
+		System.out.print("Enter Third String : ");
+		String S3 = sc.nextLine();
+		int result ; 
+		result = S1.compareToIgnoreCase(S2);
+		System.out.println("After implmenting the CompareToIgnoreCase Method , the first result is : "+result);
+		result = S1.compareToIgnoreCase(S3);
+		System.out.println("After implmenting the CompareToIgnoreCase Method , the second result is : "+result); //str1 comes before str3 in the dictionary order. Hence, str1.compareTo(str3) returns negative
+		result = S3.compareToIgnoreCase(S1);
+		System.out.println("After implmenting the CompareToIgnoreCase Method , the third result is : "+result);  //str3 comes after str1 in the dictionary order. Hence, str3.compareTo(str1) returns positive
+		
+		//TO Check Two Strings are equal or not
+		if(S1.compareToIgnoreCase(S2) == 0)
+			System.out.println("Yes , the given 2 strings are equal .");
+		else
+			System.out.println("Yes , the given 2 strings are not equal .");
+		
+		//Compare with case
+		result = S2.compareToIgnoreCase(S3);
+		System.out.println("After implmenting the CompareToIgnoreCase Method , the result is : "+result);
+		sc.close();
+		
+
+	}
+
+}
