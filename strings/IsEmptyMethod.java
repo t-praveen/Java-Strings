42e4a27cd480ed55dc91a4913be9fdfa7050b24e
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-01 16:29:59 +0530

    Add files via upload

---
+++ strings/IsEmptyMethod.java
@@ -0,0 +1,15 @@
+package strings;
+
+public class IsEmptyMethod {
+
+	public static void main(String[] args) {
+		
+		 	String str1 = "Java Programming";
+		    String str2 = "";
+		    System.out.println(str1.isEmpty());
+		    System.out.println(str2.isEmpty());
+
+		
+	}
+
+}
