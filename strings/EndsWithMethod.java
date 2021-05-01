e392e5fb888091498d22b30730c6330b445e3c9d
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-01 16:20:33 +0530

    Add files via upload

---
+++ strings/EndsWithMethod.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class EndsWithMethod {
+
+	public static void main(String[] args) {
+
+			String str = "Java Programming";
+		    System.out.println(str.endsWith("mming")); 
+		    System.out.println(str.endsWith("g")); 
+		    System.out.println(str.endsWith("a Programming"));
+		    System.out.println(str.endsWith("programming")); 
+		    System.out.println(str.endsWith("Java")); 
+
+		
+	}
+
+}
