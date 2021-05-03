274bd97e51e517d9dde67a793c65f727a3e39086
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-03 17:30:01 +0530

    Add files via upload

---
+++ strings/SubSequenceMethod.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class SubSequenceMethod {
+
+	public static void main(String[] args) {
+
+		 String str = "Java Programming";
+
+		    System.out.println(str.subSequence(3, 8)); 
+		    System.out.println(str.subSequence(5, 10));
+		
+	}
+
+}
