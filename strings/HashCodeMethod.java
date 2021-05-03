1c091cc72e5a53d38803c7869eb10bb1d01320f5
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-03 13:47:47 +0530

    Add files via upload

---
+++ strings/HashCodeMethod.java
@@ -0,0 +1,20 @@
+package strings;
+
+public class HashCodeMethod {
+
+	public static void main(String[] args) {
+		
+			    String str1 = "Java";
+			    String str2 = "Java Programming";
+			    String str3 = "";
+
+			    System.out.println(str1.hashCode()); // 2301506
+			    System.out.println(str2.hashCode()); // 1377009627
+
+			    // hash code of empty string is 0
+			    System.out.println(str3.hashCode()); // 0
+
+
+	}
+
+}
