94edd00e7db7e8e7128d26d36fe0abaeacb4617e
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-03 13:25:32 +0530

    Add files via upload

---
+++ strings/InternMethod.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class InternMethod {
+
+	public static void main(String[] args) {
+		
+
+	    String str1 = new String("xyz");
+	    String str2 = new String("xyz");
+	    System.out.println(str1 == str2);
+	    
+	    str1 = str1.intern();
+	    str2 = str2.intern();
+	    System.out.println(str1 == str2);
+	}
+
+}
