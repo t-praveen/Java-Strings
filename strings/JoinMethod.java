886a344346ad5cba1ed7b2bc5db406b99fce3c2d
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-03 17:25:24 +0530

    Add files via upload

---
+++ strings/JoinMethod.java
@@ -0,0 +1,29 @@
+package strings;
+import java.util.ArrayList ;
+
+public class JoinMethod {
+
+	public static void main(String[] args) {
+	
+		String result;
+
+	    result = String.join("-", "Java", "is", "fun");
+	    System.out.println(result);
+	    
+	    ArrayList<String> text = new ArrayList<>();
+
+	    // adding elements to the arraylist
+	    text.add("Java");
+	    text.add("is");
+	    text.add("fun");
+
+	    String result1;
+
+	    result1 = String.join("-", text);
+	    System.out.println(result1);
+
+
+		
+	}
+
+}
