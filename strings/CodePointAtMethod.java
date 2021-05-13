0bc2ff7accdd6da2e9b60a908d84435c316e859e
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-13 14:32:11 +0530

    Add files via upload

---
+++ strings/CodePointAtMethod.java
@@ -0,0 +1,14 @@
+package strings;
+
+public class CodePointAtMethod {
+
+	public static void main(String[] args) {
+
+		String myStr = "Hello";
+		int result = myStr.codePointAt(3);
+		System.out.println(result);
+
+		
+	}
+
+}
